38d9943e20b2024a2106c352d54a83f10d59fed0
Adam Hoffman <wintros@yahoo.com>
2019-07-12 12:00:24 -0600

    cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,60 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+container = document.querySelector(".cards-container")
+
+axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+.then(res => {
+    console.log(res)
+        res.data.articles.bootstrap.forEach(data =>{        
+            container.appendChild(createComponent(data))
+        res.data.articles.javascript.forEach(data => {
+            container.appendChild(createComponent(data))
+        res.data.articles.jquery.forEach(data => {
+            container.appendChild(createComponent(data))
+        res.data.articles.node.forEach(data => {
+            container.appendChild(createComponent(data))
+        res.data.articles.technology.forEach(data => {
+            container.appendChild(createComponent(data))
+                    })
+                })
+            })
+        })
+    })
+})
+    
+       
+    
+
+
+
+.catch(error => {
+    console.log("Down for Now", error)
+})
+
+function createComponent(data) {
+    const card = document.createElement("div")
+    const title = document.createElement("div")
+    title.textContent = data.headline
+
+    const author = document.createElement("div")   
+    const imgContainer = document.createElement("div")
+
+    const img = document.createElement("img")
+    img.src = data.authorPhoto
+
+    const by = document.createElement("span")
+    by.textContent = `By: ${data.authorName}`
+
+    card.append(title, author, imgContainer, img, by)
+        author.append(imgContainer, img, by)
+
+    card.classList.add("card")
+    title.classList.add("headline")
+    author.classList.add('author')
+    imgContainer.classList.add("img-container")
+    
+    return card
+}
+
+
